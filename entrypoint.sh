f4452260fa3542985d675c4158eb4ad3ea9d1f02
dbarthe <barthelemy.delemotte@gmail.com>
2018-06-18 14:25:59 +0200

    fix var name typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$1" = "--worker" ]; then
   ssh-keyscan github.com > ~/.ssh/known_hosts
-  git config --global user.name "$GIHTUB_USER"
+  git config --global user.name "$GITHUB_USER"
   git config --global user.email "$GITHUB_EMAIL"
   docker login --username="$DOCKER_HUB_USER" --password="$DOCKER_HUB_PASSWORD"
   exec npm run serve-worker
